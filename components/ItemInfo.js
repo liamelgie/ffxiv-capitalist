97499219ec9a27edc370d8f84c0f66b55452e169
Liam Elgie <liamelgie@users.noreply.github.com>
2022-05-09 17:36:28 +0100

    Update ItemInfo to avoid errors with invalid IDs

--- components/ItemInfo.js
+++ components/ItemInfo.js
@@ -7,24 +7,27 @@ const ItemInfo = (props) => {
     const { info, isLoading, isError } = useItemInfo(id)
     if (isError) return (
         <div>
-            <h2>Info</h2>
             <div>Failed to load</div>
         </div>
     )
     if (isLoading) return (
         <div>
-            <h2>Info</h2>
-            <div>Loading...</div>
+            <div>Getting item info...</div>
+        </div>
+    )
+    if (info.item.Error) return (
+        <div>
+            <div>Failed to load</div>
         </div>
     )
     return (
-        <div className={styles.itemInfoCard} style={{'width': '500px'}}>
+        <div className={styles.itemInfoCard}>
+            <h2 className={styles.itemName}>{ info.item.Name }</h2>
             <div className={styles.mainIconContainer}>
                 <img alt={`${info.item.Name} icon`} className={styles.mainIcon} src={`https://xivapi.com/${info.item.IconHD}`} />
             </div>
             <div className={styles.mainInfo}>
-                <h2>{ info.item.Name }</h2>
-                <p>{ info.item.Description }</p>
+                <p>{ info.item.Description || 'No description available.' }</p>
             </div>
             <div className={styles.miscInfo}>
                 <span>ID: { info.item.ID }</span>
