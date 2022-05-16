5d110290d0e6b577fafafddcfa0fd910b609c5dd
Liam Elgie <liamelgie@users.noreply.github.com>
2022-05-16 20:10:56 +0100

    Add placeholder tables

---
+++ components/SalesTablePlaceholder.js
@@ -0,0 +1,33 @@
+import styles from '../styles/Table.module.css'
+
+const SalesTablePlaceholder = (props) => {
+    const { limit, crossWorld } = props
+    const dummyRows = [...Array(limit).keys()].map((d, i) => {
+        return (
+            <tr key={i}>
+                <td className={styles.hqColumn}><span>P</span></td>
+                <td><span>139000</span></td>
+                <td><span>1</span></td>
+                <td><span>139000</span></td>
+                <td><span>nn hour ago</span></td>
+                {crossWorld && <td><span>{'Rowena'}</span></td>}     
+            </tr>
+        )
+    })
+    return (
+        <table className={styles.tablePlaceholder}>
+            <thead>
+                <tr>
+                    <th className={styles.hqColumn}>HQ</th>
+                    <th>Price per</th>
+                    <th>Quantity</th>
+                    <th>Total</th>
+                    <th>Date</th>
+                    {crossWorld && <th>World</th>}
+                </tr>
+            </thead>
+            <tbody>{dummyRows}</tbody>
+        </table>
+    )
+}
+export default SalesTablePlaceholder
