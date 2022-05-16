d6f1b0ce60ee880b175fa470e04f5ae393d3c71a
Liam Elgie <liamelgie@users.noreply.github.com>
2022-05-16 21:09:40 +0100

    Remove logging

--- pages/api/universalis/sales/[...param].js
+++ pages/api/universalis/sales/[...param].js
@@ -6,7 +6,6 @@ export default async function handler(req, res) {
     if (!param[0] || !param[1]) return res.status(404)
     const uni = new Universalis()
     const dataCenter = await findDataCenter(param[0])
-    console.log(param)
     try {
         const localSales = await uni.getSales(param[0], param[1])
         const sortedLocalSales = uni.sortSalesByDay(localSales)
