0a622e6cdd1892ee2e756e9f30af1776b8b5cb70
Liam Elgie <liamelgie@users.noreply.github.com>
2022-05-24 13:09:39 +0100

    Add search endpoint

--- pages/api/xiv/search.js
+++ pages/api/xiv/search.js
@@ -1,11 +1,19 @@
 const fs = require('fs');
 const XIVAPI = require('@xivapi/js')
 const xiv = new XIVAPI({private_key: process.env.XIVAPI_KEY})
+const url = 'http://localhost:3000'
 
 export default async function handler(req, res) {
-    const { string } = req.query
+    const { string, index='item' } = req.query
     if (!string) return res.status(404)
-    await xiv.search(string).then(results => {
-        res.status(200).json({ results })
+    await xiv.search(string, { indexes: index }).then(async (results) => {
+        const validIDs = await fetch(`${url}/api/universalis/marketableItems`).then(res => res.json())
+        const validatedIndexes = await Promise.all(results.Results.map(async (result) => {
+            return validIDs.includes(result.ID)
+        }))
+        const validatedResults = results.Results.filter((value, index) => validatedIndexes[index])
+        return validatedResults
+    }).then(validatedResults => {
+        res.status(200).json({ results: validatedResults })
     })
   }  
