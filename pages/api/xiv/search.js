80cde105c366c16bb25befde73b835330053d675
Liam Elgie <liamelgie@users.noreply.github.com>
2022-05-28 20:40:48 +0100

    Remove redundant validation check

--- pages/api/xiv/search.js
+++ pages/api/xiv/search.js
@@ -8,11 +8,7 @@ export default async function handler(req, res) {
     if (!string) return res.status(404)
     await xiv.search(string, { indexes: index }).then(async (results) => {
         const validIDs = await fetch(`${url}/api/universalis/marketableItems`).then(res => res.json())
-        const validatedIndexes = await Promise.all(results.Results.map(async (result) => {
-            return validIDs.includes(result.ID)
-        }))
-        const validatedResults = results.Results.filter((value, index) => validatedIndexes[index])
-        return validatedResults
+        return results.Results.filter((value, index) => validIDs.includes(value.ID))
     }).then(validatedResults => {
         res.status(200).json({ results: validatedResults })
     })
