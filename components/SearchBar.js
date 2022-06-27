56424d9a1b988c17656451be4c3b2084d30b0b88
liamelgie <liamelgie@gmail.com>
2022-06-27 20:19:41 +0100

    Add redirection to first result on enter key

--- components/SearchBar.js
+++ components/SearchBar.js
@@ -1,4 +1,5 @@
 import { useState, useCallback, useRef } from 'react'
+import { useRouter } from 'next/router'
 import { debounce } from "lodash"
 import styles from '../styles/SearchBar.module.css'
 import useSearchData from '../hooks/useSearchData'
@@ -6,6 +7,7 @@ import SearchResults from './SearchResults'
 import useOnClickOutside from '../hooks/useOnClickOutside'
 
 const SearchBar = () => {
+    const router = new useRouter()
     const [ searchParam, setSearchParam ] = useState('')
     const [ localSearchValue, setLocalSearchValue ] = useState('')
     const { data, isLoading, isError } = useSearchData(searchParam)
@@ -13,6 +15,15 @@ const SearchBar = () => {
     const [ isSearchFocused, setSearchFocused ] = useState(false)
     const focusRef = useRef()
     useOnClickOutside(focusRef, useCallback(() => setSearchFocused(false), []))
+    const handleKey = (e)  => {
+        const keyCode = e.keyCode || e.which;
+        if (keyCode === 13 && data) {
+            router.push(`/item/${data.results[0].ID}`).then(() => {
+                e.target.blur()
+                setSearchFocused(false)
+            })
+        }
+    }
     return (
         <div className={styles.container} ref={focusRef}>
             <input 
@@ -23,6 +34,7 @@ const SearchBar = () => {
                     setLocalSearchValue(event.target.value)
                     debounceOnChange(event.target.value)
                 }}
+                onKeyUp={(e) => handleKey(e)}
                 onFocus={(e) => setSearchFocused(true)}
             />
             { !!localSearchValue && isSearchFocused && <SearchResults data={data} isLoading={isLoading} isError={isError} setSearchFocused={setSearchFocused} isSearchFocused={isSearchFocused} />}
